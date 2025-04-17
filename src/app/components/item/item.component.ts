e16f68beadcbfa65e346d402eec23088c245abf1
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-17 17:40:13 -0300

    feat: incluido o loop para exibição dos itens na lista

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +9,13 @@ import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  @Input() item!: Item;
 
-  faPen = faPen;
-  faTrash = faTrash
+  faPen: IconProp = faPen as IconProp;
+  faTrash: IconProp = faTrash as IconProp;
+  iconStyle: any = {
+    color: '#859dd6'
+  }
 
   constructor() { }
 
